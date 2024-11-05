4237f8a1bc930d1d056ab370931aec6b1c4719d2
Manuel Moauro <manuelmoauro1@gmail.com>
2024-11-05 11:11:18 -0300

    Create const for storing api key

--- app/src/main/java/com/exa/pelis/data_source/MovieApi.kt
+++ app/src/main/java/com/exa/pelis/data_source/MovieApi.kt
@@ -8,25 +8,27 @@ import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+const val API_KEY = ""
+
 
 interface MovieApi {
-    @Headers("Authorization: ")
+    @Headers("Authorization: Bearer $API_KEY")
     @GET("movie/popular")
     suspend fun getPopularMovies(@Query("page") page: Int, @Query("language") language: String): Response<MovieListApiResponse>
 
-    @Headers("Authorization: ")
+    @Headers("Authorization: Bearer $API_KEY")
     @GET("movie/{movieId}")
     suspend fun getMovieDetails(@Path("movieId") movieId: Int, @Query("language") language: String): Response<MovieDetailsResponse>
 
-    @Headers("Authorization: ")
+    @Headers("Authorization: Bearer $API_KEY")
     @GET("movie/{movieId}/similar")
     suspend fun getSimilarMovies(@Path("movieId") movieId: Int, @Query("page") page: Int, @Query("language") language: String): Response<MovieListApiResponse>
 
-    @Headers("Authorization: ")
+    @Headers("Authorization: Bearer $API_KEY")
     @GET("search/movie?include_adult=true")
     suspend fun searchMovies(@Query("query") query: String, @Query("page") page: Int, @Query("language") language: String): Response<MovieListApiResponse>
 
-    @Headers("Authorization: ")
+    @Headers("Authorization: Bearer $API_KEY")
     @GET("trending/movie/week")
     suspend fun getTrendingMovies(@Query("page") page: Int, @Query("language") language: String): Response<MovieListApiResponse>
 
